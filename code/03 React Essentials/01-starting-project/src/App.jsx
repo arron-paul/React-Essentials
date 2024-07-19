805b95bc82eec234642ceb696758a4d58897e92e
Arron Paul <hello@arron.io>
2024-07-19 13:17:26 +0100

    Split up components and their styles

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,43 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
 import { CORE_CONCEPTS } from "./data";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function getRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[getRandomInt(3)];
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
-
-// Using object destructing.
-// Used in this case to destructure the first parameter of this function.
-
-// An object destructuring simply means that we can target the
-// different properties of the incoming object by name, since the
-// CORE_CONCEPTS object has properties that match the names of the props.
-
-// These curly braces have nothing to do with the CORE_CONCEPTS object passed in.
-function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header/Header";
+import CoreConcept from "./components/CoreConcept/CoreConcept";
 
 function App() {
   return (
