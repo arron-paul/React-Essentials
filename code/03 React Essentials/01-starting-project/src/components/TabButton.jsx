62e46b0288ac0821716361f45adc44ae99c8acf4
Arron Paul <hello@arron.io>
2024-07-20 14:07:04 +0100

    Split up into more components

--- code/03 React Essentials/01-starting-project/src/components/TabButton.jsx
+++
@@ -1,9 +0,0 @@
-export default function TabButton({ children, isSelected, onSelect }) {
-  return (
-    <li>
-      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
-        {children}
-      </button>
-    </li>
-  );
-}
